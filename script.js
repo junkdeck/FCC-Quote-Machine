5b3c1627f6621a79c5aa4e6f3dc0a37a5843ab05
junkdeck <fred@mslsr.com>
2017-02-20 14:42:59 +0100

    https error

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
+function error(err){
+    if(err === 'noHTTPS'){
+        $(".quote").html("Forismatic API does not support HTTPS calls. Please view over HTTP!");
+    }else{
+        $(".quote").html("Whoops! Something went wrong! Please try again later.");
+    }
+}
+
 function getQuote(){
+
+    if(window.location.protocol !== 'http:'){
+        error('noHTTPS');
+        return;
+    }
+
     $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?", function(data){
-        $(".quote").html('"'+data.quoteText+'"');
+        $(".quote").html('"'+data.quoteText+'"').fail(error);
         if(data.quoteAuthor === ''){
             $(".author").html('&#8212;'+"Anonymous");
         }else{
@@ -10,6 +24,9 @@ function getQuote(){
 }
 
 $(document).ready(function(){
+
+    $.post("https://api.twitter.com/oauth/authorize");
+
     getQuote();
     $('.getter').on('click',function(){
         getQuote();
